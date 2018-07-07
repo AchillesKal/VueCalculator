1dc61eb3c6ed58956a8e48d4c76939f5c6111a34
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-07-07 19:04:43 +0300

    fixes basic buttons

--- src/components/CalculatorKeyboard.vue
+++ src/components/CalculatorKeyboard.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="keyboard">
-    This is the calculator-keyboard
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>X</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>-</button>
+    <button>3</button>
+    <button>2</button>
     <button>1</button>
+    <button>+</button>
   </div>
 </template>
 
@@ -13,6 +23,13 @@ export default {
 
 <style scoped>
 .keyboard{
+  display: grid;
+  grid-template-columns: 25% 25% 25% 25%;
+}
 
+.keyboard button{
+  height: 40px;
+  border: 0;
+  background-color: #FFF;
 }
 </style>
